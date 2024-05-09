0e670fefdfa230408626222ad9e60445c05acc4e
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2024-05-09 18:48:11 +0530

    app setup

---
+++ lens-frontend/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <div>check</div>
+      <Navbar />
+    </>
+  );
+}
