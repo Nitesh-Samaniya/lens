47a2ea8fcac8b52c04e3344ba5308f52a990d901
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2024-05-09 18:56:43 +0530

    check deployment

--- lens-frontend/src/app/page.js
+++ lens-frontend/src/app/page.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 export default function Home() {
   return (
     <>
-      <div>check</div>
+      <div>checks</div>
       <Navbar />
     </>
   );
