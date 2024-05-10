7d90355fb92069fb4c01ba5939242db45d00f280
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2024-05-10 09:33:22 +0530

    navbar completed without theme

--- lens-frontend/src/components/Navbar.jsx
+++ lens-frontend/src/components/Navbar.jsx
@@ -1,51 +1,63 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import Logo from '../../public/nav_logo.webp';
-import Image from 'next/image';
+import React, { useState } from "react";
+import Logo from "../../public/nav_logo.webp";
+import Image from "next/image";
 import { IoMdMoon } from "react-icons/io";
 import { MdOutlineMenu } from "react-icons/md";
 
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const handleDrawer = ()=>{
-    setDrawerOpen(!drawerOpen)
-  }
+  const handleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
   return (
-    <nav className="fixed w-full h-24 shadow-xl bg-white">
-        <div className="flex justify-between items-center h-full w-full px-4 2xl: px-16">
-            <Image 
-              src={Logo}
-              alt='logo'
-              width={80}
-              height={70}
-              className='cursor-pointer'
-            />
-            <div className='hidden sm:flex'>
-              <ul className='hidden sm:flex'>
-                <li className='ml-10 hover: border-b text-xl'>MakeMyWeb.</li>
-                <li className='ml-10 hover: border-b text-xl'>Home</li>
-                <li className='ml-10 hover: border-b text-xl'>Company</li>
-                <li className='ml-10 hover: border-b text-xl'>Blogs</li>
-                <li className='ml-10 hover: border-b text-xl'><IoMdMoon size={25}/></li>
-              </ul>
-            </div>
-            <div onClick={handleDrawer} className='sm:hidden cursor-pointer pl-24'>
-              <MdOutlineMenu size={25}/>
-            </div>
+    <nav className="fixed w-full h-24 shadow-xl bg-white font-poppins">
+      <div className="flex justify-between items-center h-full w-full px-6 sm:px-18 md:px-16">
+        <Image
+          src={Logo}
+          alt="logo"
+          width={80}
+          height={70}
+          className="cursor-pointer"
+        />
+        <div className="hidden sm:flex">
+          <ul className="hidden sm:flex">
+            <li className="hover:font-semibold ml-10">MakeMyWeb.</li>
+            <li className="hover:font-semibold ml-10 text-blue-500">Home</li>
+            <li className="hover:font-semibold ml-10">Company</li>
+            <li className="hover:font-semibold ml-10">Blogs</li>
+            <li className="hover:font-semibold ml-10">
+              <IoMdMoon size={25} />
+            </li>
+          </ul>
         </div>
-        <div className={
-          drawerOpen ?
-          "fixed right-0 top-24 w-[65%] sm:hidden bg-[#ecf0f3] h-screen p-10 ease-in duration-500"
-          :
-          "fixed left-[-100%] top-24 p-10 ease-in duration-500"
-        }>
+        <div onClick={handleDrawer} className="sm:hidden cursor-pointer pl-24">
+          <MdOutlineMenu size={45} />
+        </div>
+      </div>
 
-        </div>
+      <div
+        className={
+          drawerOpen
+            ? "fixed right-0 top-24 w-[65%] sm:hidden bg-[black] h-screen p-4 ease-in duration-500"
+            : "fixed left-[-100%] top-24 p-10 ease-in-out duration-500"
+        }
+      >
+        <ul className="flex flex-col gap-6 text-xl">
+          <li className="text-white hover:font-semibold">MakeMyWeb.</li>
+          <li className="text-blue-500 hover:font-semibold">Home</li>
+          <li className="text-white hover:font-semibold">Company</li>
+          <li className="text-white hover:font-semibold">Blogs</li>
+          <li className="text-white hover:font-semibold">
+            <IoMdMoon size={25} />
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
